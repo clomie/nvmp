cd9e5754660bc06865c4e8c52d5424ca204d85b6
clomie <cclomie@gmail.com>
2018-12-16 15:09:22 +0900

    Reverse output of ls-remote command

--- bin/ListRemote.ps1
+++ bin/ListRemote.ps1
@@ -10,7 +10,7 @@ $VersionTable = ParseVersionTable (DownloadString (NodeIndexTableUrl))
 
 Write-Host "version   npm      date       lts"
 Write-Host "--------- -------- ---------- ----------"
-$VersionTable | % {
+$VersionTable[($VersionTable.length - 1) .. 0] | ForEach-Object {
   Write-Host ("{0,-9} {1,-8} {2,-10} {3}" -F $_.version, $_.npm, $_.date, $_.lts)
 }
 Write-Host;
