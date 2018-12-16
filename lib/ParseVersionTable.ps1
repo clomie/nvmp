205162beace858f168c46bffa97d980ebf099be3
clomie <cclomie@gmail.com>
2018-12-16 15:19:16 +0900

    Change filter condition for parsing version table

--- lib/ParseVersionTable.ps1
+++ lib/ParseVersionTable.ps1
@@ -14,7 +14,7 @@ function ParseVersionTable {
 
   $lines = $Response -Split "`n"
   $keys = -Split ($lines | Select-Object -First 1)
-  $result = $lines | ? { $_ -Match "^v[1-9]" } | % { ToHashmap $keys (-Split $_) }
+  $result = $lines | Where-Object { $_ -Match "win-x64-zip" } | ForEach-Object { ToHashmap $keys (-Split $_) }
 
   $result
 }
